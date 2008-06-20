148bd755b950728b8e74985f55ee3d6fa165503d
Jose Fernandez <jose@umn.edu>
2008-06-20 17:04:08 -0500

    Fixed the install.rb script

--- install.rb
+++ install.rb
@@ -2,4 +2,4 @@ require 'fileutils'
 
 umn_auth_users = File.dirname(__FILE__) + '/../../../config/umn_auth_users.yml'
 FileUtils.cp File.dirname(__FILE__) + '/umn_auth_users.yml.tpl', umn_auth_users unless File.exist?(umn_auth_users)
-puts IO.read(File.join(File.dirname(__FILE__), 'README'))
+puts IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
